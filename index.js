9577500683cd450e7f12d828cda556238594dcfa
nampereira <3504501+nampereira@users.noreply.github.com>
2020-12-16 22:52:43 -0500

    Fix chat subscribe permissions

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function generateMqttToken(req, jwt, type) {
             if (userid) {
                 const userhandle = userid + btoa(userid);
                 // receive private messages: Read
-                subs.push(`${realm}/g/c/p/${userhandle}`);
+                subs.push(`${realm}/g/c/p/${userid}`);
                 // receive open messages to everyone and/or scene: Read
                 subs.push(`${realm}/g/c/o/#`);
                 // send open messages (chat keepalive, messages to all/scene): Write
